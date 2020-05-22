81a77c4ead3463a5de8e51cd1a27777e2399bb75
Brendan Koral <hipposmelas@gmail.com>
2020-05-22 15:59:57 -0500

    Set up cluster example

--- performance/index.js
+++ performance/index.js
@@ -1,8 +1,26 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster')
 
-app.get('/', (req, res) => {
-	res.send('Hi there');
-});
+//Is the file being executed in master mode?
+if (cluster.isMaster) {
+    //Cause index.js to be executed again but in child mode
+    cluster.fork()
+} else {
+    //I'm a child and am going to act like a server 
+    const express = require('express');
+    const app = express();
 
-app.listen(3000);
+
+    function doWork(duration) {
+        const start = Date.now()
+        while (Date.now() - start < duration) {
+
+        }
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000)
+        res.send('Hi there');
+    });
+
+    app.listen(3000);
+}
